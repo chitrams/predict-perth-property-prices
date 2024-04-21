3b3435f6d062a2bbb7bf08a9cedb572098131b51
chitra.m.saraswati@gmail.com <chitra.m.saraswati@gmail.com>
2024-04-21 17:11:58 +0800

    Started making rules for #1

---
+++ scripts/rules.R
@@ -0,0 +1,14 @@
+# First, save type.
+# Starting with Unit, Apartment, House, Townhouse, Land, or Commercial Property
+# and ending in : or : and (digit)
+# but not "Land size: 620 sqm" 
+# ie Land size: ddd sqm
+
+
+# If starting with:
+# Unit
+# Apartment
+# House
+# Townhouse
+# and ends in m:,
+# save this as property_size
