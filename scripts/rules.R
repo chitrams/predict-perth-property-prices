a8059d03bbb4f7c579828b36f6bc24b56bb0f742
chitra.m.saraswati@gmail.com <chitra.m.saraswati@gmail.com>
2024-04-22 16:57:48 +0800

    Updated rules to extract cols

--- scripts/rules.R
+++ scripts/rules.R
@@ -1,9 +1,32 @@
+# Address rule -----
+
+# First col is always an address. Save to address col
+
+# Price rule -----
+
+# If starts with "Sold $", save to price col
+# In same cell, "in[::alphanum::]$" saves to date col
+
+# If exists starts with "Last Sold $", 
+# Create new column and save to price col
+# In same cell, "in[::alphanum::]$" saves to date col
+
+# Property type rule -----
+
 # First, save type.
 # Starting with Unit, Apartment, House, Townhouse, Land, or Commercial Property
 # and ending in : or : and (digit)
 # but not "Land size: 620 sqm" 
 # ie Land size: ddd sqm
 
+
+# Bed, bath, park -----
+
+# - bedrooms col
+# - bathrooms col
+# - parking col
+
+# Size -----
 
 # If starting with:
 # Unit
@@ -12,3 +35,7 @@
 # Townhouse
 # and ends in m:,
 # save this as property_size
+
+# - property_size col (the one unit or townhouse "living" size)
+# - building_size col (if block of apartments, this is the building)
+# - land_size col
